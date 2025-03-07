a1bea536369939dfe47a0db046a613ecf5378c6a
john.niu <john.niu@tineco.com>
2025-03-07 13:33:34 +0800

    feat: 修改打包配置，加快打包速度

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,11 @@
 import typescript from "rollup-plugin-typescript2";
 import { terser } from "rollup-plugin-terser";
-import copy from "rollup-plugin-copy";
 import del from "rollup-plugin-delete";
 import ora from "ora";
+import { visualizer } from "rollup-plugin-visualizer";
+import cpy from "cpy";
+
+const spinner = ora("Building...");
 
 export default {
   input: "src/index.ts", // 入口文件
@@ -17,27 +20,40 @@ export default {
     typescript({
       tsconfig: "./tsconfig.json", // 使用现有的 tsconfig.json
       useTsconfigDeclarationDir: true, // 使用 tsconfig.json 中的声明文件输出目录
+      check: false, // 禁用类型检查
+      tsconfigOverride: {
+        compilerOptions: {
+          sourceMap: false, // 关闭 sourcemap 生成
+        },
+      },
     }),
     terser(), // 可选：用于压缩输出代码
-    copy({
-      targets: [
-        { src: "templates/**/*", dest: "lib/templates" }, // 复制文件
-      ],
-      filter: (src) => !src.includes("node_modules"),
-    }),
     {
       name: "ora-loader",
       buildStart() {
-        this.spinner = ora("Building...").start();
+        spinner.start();
       },
       async generateBundle() {
         // 在所有文件生成后调用
-        this.spinner.succeed("Build completed!");
+        spinner.succeed("Build completed!");
       },
       buildError(error) {
-        this.spinner.fail("Build failed!");
-        console.error(error);
+        spinner.fail(`Build failed: ${error}`);
       },
     },
+    {
+      name: "custom-copy",
+      async buildEnd() {
+        await cpy(["templates/**/*", "!**/node_modules/**"], "lib/templates", {
+          parents: true,
+          concurrency: 8,
+          overwrite: false,
+        });
+      },
+    },
+    visualizer({
+      filename: "packAnalysis.html", // 输出的文件名
+      open: false, // 自动打开浏览器查看报告
+    }),
   ],
 };
